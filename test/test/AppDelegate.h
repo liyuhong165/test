db2e6e23e8625fc95f3aea6ae23c95c9b0b4fa16
lyh165 <liyuhong165@163.com>
2017-03-22 11:14:12 +0800

    初始化test(之前开发的)项目

---
+++ test/test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by liyuhong165 on 17/3/22.
+//  Copyright © 2017年 lyh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by liyuhong165 on 17/3/22.
+//  Copyright © 2017年 lyh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
